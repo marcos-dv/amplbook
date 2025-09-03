6ea8a95863dab4ce5af3187f5b5fe6fc51f5d5c1
Marcos Dominguez <marcos.dominguezv.dev@gmail.com>
2025-09-03 13:09:00 +0200

    Minor changes

--- upload.sh
+++ upload.sh
@@ -2,6 +2,6 @@
 cd "`dirname "$0"`"
 set -ex
 source venv/bin/activate
-python -m sphinx -b html . _build/mo-book 
-rsync -av --delete _build/mo-book/ kinsta-staging:/www/amplnewsitestaging_483/public/mo-book
-rsync -av --delete _build/mo-book/ kinsta:/www/amplnewsitestaging_483/public/mo-book
+python -m sphinx -b html . _build/ampl-book
+#rsync -av --delete _build/mo-book/ kinsta-staging:/www/amplnewsitestaging_483/public/mo-book
+#rsync -av --delete _build/mo-book/ kinsta:/www/amplnewsitestaging_483/public/mo-book
