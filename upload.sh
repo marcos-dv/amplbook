d2f03987eff278ff87408fddec1e8730edf90e1b
Marcos Dominguez <marcos.dominguezv.dev@gmail.com>
2026-05-09 20:15:26 +0200

    Update mo-book reference

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@ cd "`dirname "$0"`"
 set -ex
 source venv/bin/activate
 python -m sphinx -b html . _build/ampl-book
-#rsync -av --delete _build/mo-book/ kinsta-staging:/www/amplnewsitestaging_483/public/mo-book
-#rsync -av --delete _build/mo-book/ kinsta:/www/amplnewsitestaging_483/public/mo-book
+#rsync -av --delete _build/ampl-book/ kinsta-staging:/www/amplnewsitestaging_483/public/ampl-book
+#rsync -av --delete _build/ampl-book/ kinsta:/www/amplnewsitestaging_483/public/ampl-book
